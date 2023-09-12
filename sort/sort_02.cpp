2a2509dc6eee6a571493f680fc06bed1af0546c1
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-12 17:56:21 +0300

    Create sort_02.cpp

---
+++ sort/sort_02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	std::vector ivec{ 12, 5, 218, 9, 3, 24, -1, -9, -41, 6 };
+ //                          ------------projection----------
+	std::ranges::sort(ivec, {}, [](int x) { return std::abs(x); });
+
+	for (auto i : ivec)
+		std::cout << i << ' '; 	// -1 3 5 6 9 -9 12 24 -41 218
+}
