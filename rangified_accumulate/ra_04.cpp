bacde93e6047ba090b509f547d9644a55dc28151
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-12 17:23:47 +0300

    Create ra_04.cpp

---
+++ rangified_accumulate/ra_04.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <iostream>
+
+template<std::input_iterator Iter, std::sentinel_for<Iter> Sentinel, typename Init, typename Op = std::plus<>>
+Init Accumulate(Iter beg, Sentinel end, Init init, Op op = {})
+{
+	while (beg != end)
+	{
+		init = op(std::move(init), *beg);
+		++beg;
+	}
+	return init;
+}
+
+int main()
+{
+	std::vector vec {2, 5, 7, 9, -1, 8, -4, 3, -6};
+
+	std::cout << Accumulate(vec.begin(), vec.end(), 0);
+}
