b3bd2c3b1bb75e4cf715ac2796ea2ea70bad2c48
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-13 10:03:44 +0300

    Create sentinel_05.cpp

---
+++ sentinel/sentinel_05.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <algorithm>
+#include <ranges>
+#include <iostream>
+
+struct nullchar_t {
+	bool operator==(auto pos)const
+	{
+		return *pos == '\0';
+	}
+};
+
+inline constexpr nullchar_t nullchar;
+
+
+int main()
+{
+	const char* p = "biz bugun range kutuphanesini ogreniyoruz";
+
+
+	for (auto ptr{ p }; ptr != nullchar; ++ptr)
+		std::cout << *ptr << " ";
+
+	std::cout << "\n";
+
+	std::ranges::for_each(p, nullchar, [](char c) {std::cout.put(c); });
+};
