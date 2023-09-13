5ab8669b5ddf3af8913bd8a5e4859b0a01dbb263
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-13 12:08:51 +0300

    Create iota_03.cpp

---
+++ views/iota/iota_03.cpp
@@ -0,0 +1,18 @@
+#include <ranges>
+#include <vector>
+#include <iostream>
+
+
+int main()
+{
+	auto source = std::views::iota(10) | std::views::take(20) |
+	std::views::filter([](int v) {
+	    return v % 2 == 0;
+	    });
+	//std::vector<int> dest(source.begin(), source.end()); //not common range
+	std::vector<int> dest;
+	std::ranges::copy(source, std::back_inserter(dest));
+	
+	for (auto i : dest)
+		std::cout << i << ' ';
+}
