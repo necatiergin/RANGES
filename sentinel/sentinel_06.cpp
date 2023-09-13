3af062ebefdd82e2c3d67f7e19e0669b0baacb69
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-13 10:04:16 +0300

    Create sentinel_06.cpp

---
+++ sentinel/sentinel_06.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <random>
+#include <ranges>
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	
+	vector<int> ivec(100);
+	iota(ivec.begin(), ivec.end(), 0);
+	mt19937 eng{ random_device{}() };
+	shuffle(ivec.begin(), ivec.end(), eng);
+
+	//auto iter = ranges::find(ivec, 67);
+	auto iter = ranges::find(ivec.begin(), unreachable_sentinel, 56);
+
+	cout << iter - ivec.begin();
+}
