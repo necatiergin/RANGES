45cbc8164078184f0eadd500f7eefc145048dca8
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-13 12:56:05 +0300

    Create comp_01b.cpp

---
+++ composition/comp_01b.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include <ranges>
+
+
+int main()
+{
+    using namespace std;
+
+    vector source = {4, 3, 6, 2, 12, 7, 9, 24, 15, 7, 21};
+    auto v =  source
+        | views::filter([](const int n) {return n % 3 == 0; })
+        | views::transform([](const int n) {return n * n; }) ;
+    
+    vector<int> dest { v.begin(), v.end() };
+}
