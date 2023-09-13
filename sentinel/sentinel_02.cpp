96e7ffb7a4a33d69ae1debb3be940aa56971a540
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-13 10:01:54 +0300

    Create sentinel_02.cpp

---
+++ sentinel/sentinel_02.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+template<auto EndVal>
+struct EndValue
+{
+	constexpr bool operator==(auto pos)const
+	{
+		return *pos == EndVal;
+	}
+};
+
+
+int main()
+{
+	using namespace std;
+
+	vector ivec{ 12,59,78,96,46,-1,123,74,63,71 };
+
+	ranges::sort(ivec.begin(), EndValue<-1>{});
+
+	for (auto i : ivec)
+		cout << i << ' ';    // output : 12 46 59 78 96 -1 123 74 63 71      sorted until -1
+}
