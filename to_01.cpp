5610973f7016af337ddfea3a010ba6759f7f6a30
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-14 16:53:25 +0300

    Create to_01.cpp

---
+++ to_01.cpp
@@ -0,0 +1,18 @@
+//C++ 23
+
+#include <vector>
+#include <ranges>
+#include <iostream>
+
+int main() 
+{
+	std::vector ivec{ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37 };
+
+	auto destvec = ivec | std::views::filter([](int x) {return x > 10 && x < 20; })
+		| std::views::transform([](int x) {return x * 10; })
+		| std::ranges::to<std::vector<int>>();
+
+	for (auto val : destvec)
+		std::cout << val << ' ';
+
+}
