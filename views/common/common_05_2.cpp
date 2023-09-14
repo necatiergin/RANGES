0afd11efcfe9487a73e3fbba6a7c53f423e8d9bd
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-14 16:25:38 +0300

    Create common_05_2.cpp

---
+++ views/common/common_05_2.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <ranges>
+#include <numeric>
+#include <iostream>
+
+int main()
+{
+    std::vector ivec{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
+    
+    auto rng = ivec | std::views::take_while([](int x) {return x % 20;});
+
+    auto sum = std::accumulate(rng.begin(), rng.end(), 0); //error
+    //...
+}
