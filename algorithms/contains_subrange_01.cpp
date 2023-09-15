79622c6cc504c47da5a85054976a9d755f37f61c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-15 11:05:46 +0300

    Create contains_subrange_01.cpp

---
+++ algorithms/contains_subrange_01.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <ranges>
+#include <iostream>
+#include <algorithm>
+
+//C++23
+
+int main()
+{
+	namespace rng = std::ranges;
+	
+	std::vector vec{ 2, 3, 5, 7, 9, 11, 13, 17, 19, 23, 29, 31 };
+	boolalpha(std::cout);
+	const std::vector v{ 11, 13, 17 };
+
+	std::cout << rng::contains_subrange(vec, v) << '\n';
+	std::cout << rng::contains_subrange(vec, auto({19, 23})) << '\n';
+	std::cout << rng::contains_subrange(vec, auto({ 5, 6, 7 })) << '\n';
+}
