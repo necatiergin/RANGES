65b10d0e309f6948f651111aa82c92cfe2081523
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-15 11:36:23 +0300

    Create subrange_07.cpp

---
+++ subrange/subrange_07.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <ranges>
+#include <list>
+
+int main()
+{
+	namespace rng = std::ranges;
+	
+	std::vector ivec{ 2, 3, 5, 7, 9, 11, 13, 17, 19, 23, 29, 31 };
+	std::list ilist{ 2, 3, 5, 7, 9, 11, 13, 17, 19, 23, 29, 31 };
+	auto rng1 = std::views::counted(ivec.begin(), 5);
+	//rng1 is a span
+	auto rng2 = std::views::counted(ilist.begin(), 5);
+	//rng is a subrange
+}
