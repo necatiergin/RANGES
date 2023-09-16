6af2902a0d889135f34ad0d4c3700a7e9cc8b40c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-16 05:51:02 +0300

    Update and rename rc_01.cpp to sized_range.cpp

---
+++ range_concepts/sized_range.cpp
@@ -0,0 +1,14 @@
+//source:  cppreference.com
+// https://en.cppreference.com/w/cpp/ranges/sized_range
+
+#include <forward_list>
+#include <list>
+#include <ranges>
+ 
+static_assert
+(
+    std::ranges::sized_range<std::list<int>> and
+    not std::ranges::sized_range<std::forward_list<int>>
+);
+ 
+int main() {}
