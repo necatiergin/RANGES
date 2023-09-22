f6913959a432a88578087af616f6fbaadfcf00f0
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-22 10:10:06 +0300

    Update adjacent_02.cpp

--- views/adjacent/adjacent_02.cpp
+++ views/adjacent/adjacent_02.cpp
@@ -11,12 +11,13 @@ int main(void)
 
 	vector<string> svec{ "murat", "mert", "gul",
 		"nihat", "cevahir", "jale", "seyhan" };
-	
+
 	ranges::copy(svec, ostream_iterator<string>(cout, " "));
 	std::cout << '\n';
 
-	for (auto rn : std::views::pairwise(svec)) { 
-	//for (auto rn : std::views::adjacent<3>(svec)) { 
+	for (auto rn : std::views::pairwise(svec)) {
+		//for (auto rn : std::views::adjacent<2>(svec)) { 
+		static_assert(same_as<decltype(rn), tuple<string&, string &>>); //holds
 		const auto& [x, y] = rn;
 		cout << x << ' ' << y << '\n';
 	}
