73e22ff23b044fed02843800145944d8a903ad7a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-08-09 11:09:28 +0300

    Update dangling_01.cpp

--- basic/dangling_01.cpp
+++ basic/dangling_01.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <ranges>
 #include <vector>
-#include "nutility.h"
+#include <print>
+#include <iterator>
 
 int main()
 {
 	using namespace std;
 
-	vector<int> ivec;
-	generate_n(back_inserter(ivec), 20, Irand(0, 25));
+	vector<int> ivec{ 9, 7, 3, 4, 1, 6, 7, 12, 78, 25 };
 
-	print(ivec);
-
+	//print("{}\n", ivec); //C++23
+	ranges::copy(ivec, ostream_iterator<int>{cout, " "});
+	cout << '\n';
 	auto iter1 = ranges::min_element(ivec);
-	cout << "min = " << *iter1 << " iter = " << iter1 - ivec.begin() << '\n';
+	cout << "min = " << *iter1 << "idx for min = " << iter1 - ivec.begin() << '\n';
 
 	auto iter2 = ranges::min_element(vector{ 1,2,3,4,5 }); // vector{1,2,3,4,5} is rvalue expr.
-	cout << "min = " << *iter2 << " iter = " << iter1 - ivec.begin() << '\n'; // invalid => *iter2
+	//cout << "min = " << *iter2 << " iter = " << iter1 - ivec.begin() << '\n'; // invalid => *iter2
 }
