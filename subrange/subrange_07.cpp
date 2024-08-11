7bd83f53fe450033ae4bf8ab610f6e3e5eb085ab
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-08-12 00:00:01 +0300

    Update subrange_07.cpp

--- subrange/subrange_07.cpp
+++ subrange/subrange_07.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <ranges>
 #include <list>
+#include <concepts>
 
 int main()
 {
@@ -8,6 +9,7 @@ int main()
 	std::list ilist{ 2, 3, 5, 7, 9, 11, 13, 17, 19, 23, 29, 31 };
 	auto rng1 = std::views::counted(ivec.begin(), 5);
 	//rng1 is a span
+	static_assert(std::same_as<decltype(rng1), std::span<int, 4294967295u>>);
 	auto rng2 = std::views::counted(ilist.begin(), 5);
 	//rng is a subrange
 }
