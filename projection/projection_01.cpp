acb1213b4066b8cfb15746063cdf1556580eba25
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-08-04 16:54:24 +0300

    Update projection_01.cpp

--- projection/projection_01.cpp
+++ projection/projection_01.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <format>
 
 struct Person {
     int m_id;
@@ -11,24 +12,39 @@ struct Person {
 int main()
 {
     using namespace std::literals;
-    
-    std::vector<Person> users{ 
+
+    std::vector<Person> users{
         {23, "Kagan Aslan"},
-        {37, "Necati Ergin"}, 
-        {19, "Ali Serce"}, 
-        {41, "Oguz Karan"}, 
-        {18, "Guray Sonmez"} 
-    
+        {37, "Necati Ergin"},
+        {19, "Ali Serce"},
+        {41, "Oguz Karan"},
+        {18, "Guray Sonmez"}
+
     };
 
     std::ranges::sort(users, {}, &Person::m_id);
-    if (auto iter = std::ranges::find(users, "Necati Ergin"s, &Person::m_name); iter != users.end()) {
+
+    for (const auto& per : users) {
+        std::cout << std::format("{:<16} {}\n", per.m_name, per.m_id);
+    }
+    std::cout << "\n\n";
+
+    std::ranges::sort(users, {}, &Person::m_name);
+
+    for (const auto& per : users) {
+        std::cout << std::format("{:<16} {}\n", per.m_name, per.m_id);
+    }
+
+        if (auto iter = std::ranges::find(users, "Necati Ergin"s, &Person::m_name); iter != users.end()) {
         std::cout << "found: " << iter->m_id << ' ' << iter->m_name;
     }
     else {
         std::cout << "not found\n";
     }
 }
+
+//------------------------------------------------------
+//------------------------------------------------------
 
 
 
