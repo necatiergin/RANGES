b9e82ff3b6be42b7b925bb43beb6d02753a252b6
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-08-04 12:30:56 +0300

    Update subrange_04.cpp

--- subrange/subrange_04.cpp
+++ subrange/subrange_04.cpp
@@ -5,17 +5,16 @@
 
 int main()
 {
-	std::vector ivec{ 2, 3, 5, 7, 11, 13}; 
+	std::vector ivec{ 2, 3, 5, 7, 11, 13 };
 
 	std::ranges::subrange sbvec{ next(ivec.begin()), prev(ivec.end()) };
 	constexpr bool b1 = std::ranges::sized_range<decltype(sbvec)>; //true
 
-	std::cout << "b1 = " << b1 << '\n';
-	std::list ilist{ 2, 3, 5, 7, 11, 13}; 
-	
-	std::ranges::subrange sblist{ilist.begin(), ilist.end(), ilist.size()};
+	std::cout << std::boolalpha << "b1 = " << b1 << '\n';
+	std::list ilist{ 2, 3, 5, 7, 11, 13 };
+
+	std::ranges::subrange sblist{ ilist.begin(), ilist.end(), ilist.size() };
 	//std::ranges::subrange sblist{ next(ilist.begin()), prev(ilist.end()) };
 	constexpr bool b2 = std::ranges::sized_range<decltype(sblist)>; //true
 	std::cout << "b2 = " << b2 << '\n';
 }
-
