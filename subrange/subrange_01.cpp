548b9add8c5449e87e020c981f353ae9ad20b76e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-08-04 12:23:19 +0300

    Update subrange_01.cpp

--- subrange/subrange_01.cpp
+++ subrange/subrange_01.cpp
@@ -5,25 +5,24 @@
 
 int main()
 {
-	using namespace std;
+	std::vector ivec{ 1, 2, 3, 4, 5 };
 
-	vector ivec{ 1, 2, 3, 4, 5 };
-
-	ranges::subrange sr1{ ivec }; // CTAD
+	std::ranges::subrange sr1{ ivec }; // CTAD
 
 	//subrange is a view
-	cout << "sizeof(sr1) = " << sizeof(sr1) << '\n';
-	static_assert(ranges::view<decltype(sr1)>);
+	std::cout << "sizeof(sr1) = " << sizeof(sr1) << '\n';
+	static_assert(std::ranges::view<decltype(sr1)>);
 
 	auto& r1 = sr1.front(); //valid
 	auto& r2 = sr1.back(); //valid
 
-	cout << r1 << ' ' << r2 << '\n';
+	std::cout << r1 << ' ' << r2 << '\n';
 
-	ranges::subrange sr2{ ivec.begin(), ivec.end() };
+	std::ranges::subrange sr2{ ivec.begin(), ivec.end() };
 
 	for (const auto i : sr2)
-		cout << i << ' '; //1,2,3,4,5 
-	
-	cout << std::format("\n{}\n{}\n", sr1, sr2); //C++23
+		std::cout << i << ' '; //1,2,3,4,5 
+
+	std::cout << std::format("\n{}\n{}\n", sr1, sr2); //C++23
 }
+
