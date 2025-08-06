0bdd7cbc3d9f8aa3eea7d51e1b5cb45ee1c6598d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-08-06 17:48:57 +0300

    Update counted_02.cpp

--- views/counted_02.cpp
+++ views/counted_02.cpp
@@ -9,21 +9,22 @@
 
 int main()
 {
-	using namespace std;
+	namespace rng = std::ranges;
+	namespace vw = rng::views;
 
-	std::vector vec{ 3, 1, 23, 4, 7, 9, 11, 2, 6, 5, 8, 0, 28};
-	ranges::copy(vec, ostream_iterator<int>{cout, " "});
-	cout << '\n';
+	std::vector vec{ 3, 1, 23, 4, 7, 9, 11, 2, 6, 5, 8, 0, 28 };
+	rng::copy(vec, std::ostream_iterator<int>{std::cout, " "});
+	std::cout << '\n';
 
-	auto rv1 = ranges::views::counted(vec.begin(), 3);
-	ranges::copy(rv1, ostream_iterator<int>{cout, " "});
-	cout << '\n';
+	auto rv1 = vw::counted(vec.begin(), 3);
+	rng::copy(rv1, std::ostream_iterator<int>{std::cout, " "});
+	std::cout << '\n';
 
-	auto rv2 = ranges::views::counted(vec.begin() + 3, 4);
-	ranges::copy(rv2, ostream_iterator<int>{cout, " "});
-	cout << '\n';
+	auto rv2 = vw::counted(vec.begin() + 3, 4);
+	rng::copy(rv2, std::ostream_iterator<int>{std::cout, " "});
+	std::cout << '\n';
 
-	auto rv3 = ranges::views::counted(vec.rbegin(), 3);
-	ranges::copy(rv3, ostream_iterator<int>{cout, " "});
-	cout << '\n';
+	auto rv3 = vw::counted(vec.rbegin(), 3);
+	rng::copy(rv3, std::ostream_iterator<int>{std::cout, " "});
+	std::cout << '\n';
 }
