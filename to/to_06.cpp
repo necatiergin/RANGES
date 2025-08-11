7b7757fb3b3edb21600b7a3e1a85ef1b6985f840
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-08-11 12:50:46 +0300

    Create to_06.cpp

---
+++ to/to_06.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <string>
+#include <ranges>
+
+#include <map>
+#include <ranges>
+#include <iostream>
+
+
+int main() 
+{
+    std::vector<std::pair<int, std::string>> myvec = {
+        {1, "one"}, {2, "two"}, {3, "three"}, {4, "four"}
+    };
+
+    auto mymap = myvec | std::ranges::to<std::map>();
+
+    for (const auto& [k, v] : mymap)
+        std::cout << k << " -> " << v << '\n';
+}
