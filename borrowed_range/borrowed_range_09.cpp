adab5a52d49714a1e313e3474e8bcbd49b51e902
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-08-11 18:02:51 +0300

    Create borrowed_range_09.cpp

---
+++ borrowed_range/borrowed_range_09.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <ranges>
+#include <vector>
+#include <print>
+#include <iterator>
+#include <algorithm>
+
+int main()
+{
+	using namespace std;
+
+	vector<int> ivec{ 9, 7, 3, 4, 1, 6, 7, 12, 78, 25 };
+
+	//print("{}\n", ivec); //C++23
+	ranges::copy(ivec, ostream_iterator<int>{cout, " "});
+	cout << '\n';
+	auto iter1 = ranges::min_element(ivec);
+	cout << "min = " << *iter1 << "idx for min = " << iter1 - ivec.begin() << '\n';
+
+	auto iter2 = ranges::min_element(vector{ 1,2,3,4,5 }); // vector{1,2,3,4,5} is rvalue expr.
+	//cout << "min = " << *iter2 << " iter = " << iter1 - ivec.begin() << '\n'; // invalid => *iter2
+}
